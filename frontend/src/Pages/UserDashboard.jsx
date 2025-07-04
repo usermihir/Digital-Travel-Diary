2be7d6d7550a69dc720c888b4a625fb9a0fc390e
usermihir <mihirkumarb813@gmail.com>
2025-07-04 17:08:03 +0530

    Fix case-sensitive import path

--- frontend/src/Pages/UserDashboard.jsx
+++ frontend/src/Pages/UserDashboard.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
 import DashboardContent from "../components/DashboardContent";
-import "../styles/UserDashboard.css";
+import "../Styles/UserDashboard.css";
 
 function UserDashboard() {
   const { id } = useParams();
